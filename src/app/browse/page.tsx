f326978cbedfbe34310851ad7b7352e08ad924da
puayongxiang <puayongxiang@gmail.com>
2026-03-24 06:40:24 +0800

    Always require editor access for browse page

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -3,14 +3,10 @@ import BrowseClient from "./BrowseClient";
 import { getAuthenticatedUser } from "@/lib/auth";
 import { isEditorEmail } from "@/lib/editorAccess";
 
-const enableInternalTools = process.env.ENABLE_INTERNAL_TOOLS === "1";
-
 export default async function BrowsePage() {
-  if (!enableInternalTools) {
-    const user = await getAuthenticatedUser();
-    if (!isEditorEmail(user?.email)) {
-      redirect("/");
-    }
+  const user = await getAuthenticatedUser();
+  if (!isEditorEmail(user?.email)) {
+    redirect("/");
   }
 
   return <BrowseClient />;
